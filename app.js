3cbe8574d434f5752677224951ff825ba45c1a40
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2024-12-13 12:33:02 +0400

    use lodash unic function

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const os = require("os");
 const _ = require("lodash");
 const notes = require("./notes.js");
 
-var res = notes.add(9, -2);
-console.log("Result:", res);
+// console.log(_.isString(true));
+// console.log(_.isString("Irakli"));
+var filteredArray = _.uniq(["Irakli", 1, "Irakli", 1, 2, 3, 4]);
+console.log(filteredArray);
+
+// var res = notes.add(9, -2);
+// console.log("Result:", res);
 // var user = os.userInfo();
 // fs.appendFile(
 //   "greetings.txt",
