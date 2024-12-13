e1723e8ea6ea78cff8ed0da0d4bafafca565e167
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2024-12-13 13:46:42 +0400

    make project ready for section of Getting user input
    some staff that was used for excercise purpuses has been removed

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
 console.log("Starting app.js");
 
 const fs = require("fs");
-const os = require("os");
 const _ = require("lodash");
+
 const notes = require("./notes.js");
 
-// console.log(_.isString(true));
-// console.log(_.isString("Irakli"));
-var filteredArray = _.uniq(["Ambroladze"]);
-console.log(filteredArray);
-
-// var res = notes.add(9, -2);
-// console.log("Result:", res);
-// var user = os.userInfo();
-// fs.appendFile(
-//   "greetings.txt",
-//   `Hello ${user.username}! You are ${notes.age} years old!`,
-//   function (err) {
-//     if (err) {
-//       console.log("Unable to write to file");
-//     }
-//   }
-// );
+console.log(notes.addNote());
