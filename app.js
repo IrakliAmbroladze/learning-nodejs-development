c767d94e07251dddefd446ae064d6b6dd71f107f
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2024-12-18 18:26:53 +0400

    feat: add yargs in code

--- app.js
+++ app.js
@@ -2,22 +2,24 @@ console.log("Starting app.js");
 
 const fs = require("fs");
 const _ = require("lodash");
+const yargs = require("yargs");
 
 const notes = require("./notes.js");
 
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log("Command: ", command);
+console.log("Process", process.argv);
+console.log("Yargs", argv);
 
 if (command === "add") {
   console.log("Adding new note");
 } else if (command === "list") {
   console.log("Listing all notes");
 } else if (command === "read") {
-  console.log("Reading a note");
-} else if (command === "list") {
-  console.log("Removing a note");
+  console.log("Reading note");
+} else if (command === "remove") {
+  console.log("Removing note");
 } else {
   console.log("Command not recognized");
 }
-
-console.log(process.argv);
