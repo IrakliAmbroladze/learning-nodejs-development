526a6fff7cda74b354082de36faa54f1f6c85f00
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2024-12-29 01:54:25 +0400

    ch3/Trying and catching code block

--- notes.js
+++ notes.js
@@ -1,6 +1,19 @@
 console.log("Starting notes.js");
 
+const fs = require("fs");
+
 var addNote = (title, body) => {
+  var notes = [];
+  try {
+    var notesString = fs.readFileSync("notes-data.json");
+    notes = JSON.parse(notesString);
+  } catch (error) {}
+  var note = {
+    title,
+    body,
+  };
+  notes.push(note);
+  fs.writeFileSync("notes-data.json", JSON.stringify(notes));
   console.log("Adding note", title, body);
 };
 
