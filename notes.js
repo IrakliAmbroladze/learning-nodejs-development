08c0c6e2493809273722b001afabfc03ca0ae901
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2024-12-30 00:10:30 +0400

    ch3/Working with fetchNotes

--- notes.js
+++ notes.js
@@ -2,17 +2,21 @@ console.log("Starting notes.js");
 
 const fs = require("fs");
 
-var addNote = (title, body) => {
-  var notes = [];
+var fetchNotes = () => {
   try {
     var notesString = fs.readFileSync("notes-data.json");
-    notes = JSON.parse(notesString);
-  } catch (error) {}
-  var duplicateNotes = notes.filter((note) => note.title === title);
+    return JSON.parse(notesString);
+  } catch (error) {
+    return [];
+  }
+};
+var addNote = (title, body) => {
+  var notes = fetchNotes();
   var note = {
     title,
     body,
   };
+  var duplicateNotes = notes.filter((note) => note.title === title);
   if (duplicateNotes.length === 0) {
     notes.push(note);
     fs.writeFileSync("notes-data.json", JSON.stringify(notes));
