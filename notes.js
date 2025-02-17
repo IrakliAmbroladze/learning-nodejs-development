f02349cfe89e6576420016e8f11f83da75eb1be6
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-17 23:57:11 +0400

    remove note in notes.js
    started writing remove note code. Till now, i have written two lines: 1. fetching data and 2. filtering it. now i have to write code for saving notes array. I am on page 198.

--- notes.js
+++ notes.js
@@ -40,7 +40,7 @@ var getNote = (title) => {
 var removeNote = (title) => {
   var notes = fetchNotes();
   var filteredNotes = notes.filter((note) => note.title !== title);
-  return filteredNotes;
+  // return filteredNotes;
 };
 
 module.exports = {
