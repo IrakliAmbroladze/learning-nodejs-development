df0022812206f2a45384cbbd831851ba4f2ffbcb
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-03-16 09:36:09 +0400

    ch5/illustrating async. setTImeout 0

--- weather-app/playground/async-basics.js
+++ weather-app/playground/async-basics.js
@@ -2,4 +2,7 @@ console.log("Starting app");
 setTimeout(() => {
   console.log("Inside of callback");
 }, 2000);
+setTimeout(() => {
+  console.log("Second setTimeout");
+}, 0);
 console.log("Finishing up");
