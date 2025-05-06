e20c3177c4a4dd0d91db2cf93e7e7dad6584fba9
Irakli <ambroladzeirakli@gmail.com>
2025-05-06 08:10:13 +0400

    ch5/final console address latitude and longitude

--- weather-app/app.js
+++ weather-app/app.js
@@ -12,6 +12,8 @@ request(
     json: true,
   },
   (error, response, body) => {
-    console.log("Response Body:", JSON.stringify(response, null, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
   }
 );
